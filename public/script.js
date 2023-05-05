65b72be332015d7770ffc24057215753bb67d098
Femi Fatokun <76601789+talibackend@users.noreply.github.com>
2023-05-05 15:05:51 +0100

    Can now collect stream

--- public/script.js
+++ public/script.js
@@ -1,1 +1,8 @@
-console.log('Hello world...');
+let videoElement = document.getElementById("video-script");
+
+let localStream;
+
+navigator.mediaDevices.getUserMedia({ video : true }).then((stream)=>{
+    localStream = stream;
+    videoElement.srcObject = stream;
+}).catch(err=>{ console.log(err) });
