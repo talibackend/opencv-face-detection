c4a6292a489c0dfb49a9df25d6a48219c2ecf9bb
Femi Fatokun <76601789+talibackend@users.noreply.github.com>
2023-05-12 15:32:04 +0100

    Done with basic design

--- public/script.js
+++ public/script.js
@@ -5,4 +5,10 @@ let localStream;
 navigator.mediaDevices.getUserMedia({ video : true }).then((stream)=>{
     localStream = stream;
     videoElement.srcObject = stream;
+    console.log(stream);
 }).catch(err=>{ console.log(err) });
+
+setTimeout(()=>{
+    // localStream.onaddtrack((track)=>{ console.log(track) });
+    // console.log(localStream.getVideoTracks());
+}, 2000);
